475f89f08a85eceaf213989471b34eca8edd3ead
Noah Holoubek <noahholo@icloud.com>
2022-02-07 20:58:46 -0600

    Optimize spacing of Minimal App Bar

--- lib/widgets/minimal_app_bar.dart
+++ lib/widgets/minimal_app_bar.dart
@@ -14,7 +14,8 @@ class MinimalAppBar extends StatelessWidget implements PreferredSizeWidget {
     this.leftAction,
     this.rightIcon,
     this.rightAction,
-  });
+    Key? key,
+  }) : super(key: key);
 
   @override
   Size get preferredSize => Size.fromHeight(height);
@@ -27,7 +28,7 @@ class MinimalAppBar extends StatelessWidget implements PreferredSizeWidget {
         fit: StackFit.loose,
         children: [
           Align(
-            alignment: const Alignment(0, 0.5),
+            alignment: const Alignment(0, 1.8),
             child: SizedBox(
               height: MediaQuery.of(context).size.height / 13,
               width: MediaQuery.of(context).size.width / 5,
@@ -38,47 +39,45 @@ class MinimalAppBar extends StatelessWidget implements PreferredSizeWidget {
                   fontSize: SizeConfig.blockSizeVertical * 2.5,
                 ),
                 textAlign: TextAlign.center,
-              )
+              ),
             ),
           ),
-          if ( leftIcon != null && leftAction != null )
-            Align(
-              alignment: const Alignment(-0.9, 0),
-              child: SizedBox(
-                height: MediaQuery.of(context).size.height / 15,
-                width: MediaQuery.of(context).size.width / 10,
-                child: Ink(
-                  decoration: ShapeDecoration(
-                    color: Theme.of(context).colorScheme.primary.withOpacity(0.7),
-                    shape: const CircleBorder(),
-                  ),
-                  child: IconButton(
-                    icon: Icon(leftIcon),
-                    color: Theme.of(context).colorScheme.onPrimary,
-                    onPressed: leftAction,
-                  ),
+          if ( leftIcon != null && leftAction != null ) Align(
+            alignment: const Alignment(-0.9, 0.7),
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height / 15,
+              width: MediaQuery.of(context).size.width / 10,
+              child: Ink(
+                decoration: ShapeDecoration(
+                  color: Theme.of(context).colorScheme.primary.withOpacity(0.7),
+                  shape: const CircleBorder(),
+                ),
+                child: IconButton(
+                  icon: Icon(leftIcon),
+                  color: Theme.of(context).colorScheme.onPrimary,
+                  onPressed: leftAction,
                 ),
               ),
             ),
-          if ( rightIcon != null && rightAction != null )
-            Align(
-              alignment: const Alignment(0.9, 0),
-              child: SizedBox(
-                height: MediaQuery.of(context).size.height / 15,
-                width: MediaQuery.of(context).size.width / 10,
-                child: Ink(
-                  decoration: ShapeDecoration(
-                    color: Theme.of(context).colorScheme.primary.withOpacity(0.7),
-                    shape: const CircleBorder(),
-                  ),
-                  child: IconButton(
-                    icon: const Icon(Icons.chevron_left),
-                    color: Theme.of(context).colorScheme.onPrimary,
-                    onPressed: rightAction,
-                  ),
+          ),
+          if ( rightIcon != null && rightAction != null ) Align(
+            alignment: const Alignment(0.9, 0.7),
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height / 15,
+              width: MediaQuery.of(context).size.width / 10,
+              child: Ink(
+                decoration: ShapeDecoration(
+                  color: Theme.of(context).colorScheme.primary.withOpacity(0.7),
+                  shape: const CircleBorder(),
+                ),
+                child: IconButton(
+                  icon: const Icon(Icons.chevron_left),
+                  color: Theme.of(context).colorScheme.onPrimary,
+                  onPressed: rightAction,
                 ),
               ),
             ),
+          ),
         ],
       ),
     );
