52a350defd122ada6212a1e0b6478d79ade14d17
Noah Holoubek <noahholo@icloud.com>
2022-02-07 20:57:25 -0600

    Create cache managers

---
+++ lib/providers/staff_cache_manager.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+
+class StaffCacheManager extends CacheManager {
+
+  static const key = 'staffCache';
+  static const Duration cacheTimeout = Duration(days: 15);
+
+  StaffCacheManager() : super(
+    Config(
+      key,
+      stalePeriod: cacheTimeout,
+      maxNrOfCacheObjects: 5,
+      fileService: HttpFileService(),
+    ),
+  );
+}
---
+++ lib/providers/staff_image_cache_manager.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+
+class StaffImageCacheManager extends CacheManager {
+
+  static const key = 'staffImageCache';
+  static const Duration cacheTimeout = Duration(days: 30);
+
+  StaffImageCacheManager() : super(
+    Config(
+      key,
+      stalePeriod: cacheTimeout,
+      maxNrOfCacheObjects: 100,
+    ),
+  );
+
+}
