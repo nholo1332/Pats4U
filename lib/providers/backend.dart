41979be5562b9bd8907d5d74a6fa5bf0e97bc3a6
Noah Holoubek <noahholo@icloud.com>
2022-02-09 10:23:30 -0600

    Add option to force refresh data
    And add function to pull classes

--- lib/providers/backend.dart
+++ lib/providers/backend.dart
@@ -1,13 +1,16 @@
 import 'dart:convert';
 
+import 'package:pats4u/models/class.dart';
 import 'package:pats4u/models/staff_member.dart';
 import 'package:pats4u/providers/staff_cache_manager.dart';
 
 class Backend {
-
   static const String baseURL = 'http://noahs-macbook-pro.local:3000';
 
-  static Future<List<StaffMember>> getStaffMembers() {
+  static Future<List<StaffMember>> getStaffMembers({bool force = false}) async {
+    if ( force ) {
+      await StaffCacheManager().emptyCache();
+    }
     return StaffCacheManager().getSingleFile(baseURL + '/staff/all').then((value) async {
       if ( await value.exists() ) {
         var res = await value.readAsString();
@@ -18,5 +21,14 @@ class Backend {
     });
   }
 
-
+  static Future<List<Class>> getClasses() {
+    return StaffCacheManager().getSingleFile(baseURL + '/class/all').then((value) async {
+      if ( await value.exists() ) {
+        var res = await value.readAsString();
+        return (json.decode(res) as List).map((s) => Class.fromJson(s)).toList();
+      } else {
+        return [];
+      }
+    });
+  }
 }
