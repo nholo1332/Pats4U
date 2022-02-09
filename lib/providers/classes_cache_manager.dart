ff9c2e673f003f8c30b06626bd679b3071dc72dc
Noah Holoubek <noahholo@icloud.com>
2022-02-09 10:22:36 -0600

    Create classes_cache_manager.dart

---
+++ lib/providers/classes_cache_manager.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+
+class ClassesCacheManager extends CacheManager {
+
+  static const key = 'classesCache';
+  static const Duration cacheTimeout = Duration(days: 15);
+
+  ClassesCacheManager() : super(
+    Config(
+      key,
+      stalePeriod: cacheTimeout,
+      maxNrOfCacheObjects: 5,
+      fileService: HttpFileService(),
+    ),
+  );
+}
