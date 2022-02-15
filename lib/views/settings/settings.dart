74ea872dd9c4314f1dae0930882efcc44cb86ccb
Noah Holoubek <noahholo@icloud.com>
2022-02-15 14:41:13 -0600

    Add cache options to settings view

--- lib/views/settings/settings.dart
+++ lib/views/settings/settings.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:pats4u/providers/auth.dart';
+import 'package:pats4u/providers/classes_cache_manager.dart';
 import 'package:pats4u/providers/constants.dart';
+import 'package:pats4u/providers/events_cache_manager.dart';
+import 'package:pats4u/providers/menu_cache_manager.dart';
+import 'package:pats4u/providers/menu_image_cache_manager.dart';
 import 'package:pats4u/providers/size_config.dart';
+import 'package:pats4u/providers/staff_cache_manager.dart';
+import 'package:pats4u/providers/staff_image_cache_manager.dart';
 import 'package:pats4u/views/bug_report/bug_report.dart';
 import 'package:pats4u/views/privacy_policy/privacy_policy.dart';
 import 'package:pats4u/widgets/minimal_app_bar.dart';
@@ -316,6 +322,188 @@ class _SettingsState extends State<Settings> {
             ),
           ],
         ),
+      ),
+    );
+    items.add(
+      const SizedBox(
+        height: 25,
+      ),
+    );
+    items.add(
+      Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 25,
+            ),
+            child: Text(
+              'Cache',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: SizeConfig.blockSizeVertical * 2.2,
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+    items.add(
+      Padding(
+        padding: const EdgeInsets.only(
+          left: 25,
+          right: 25,
+          top: 10,
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Text(
+                'The application cache allows data to be stored locally on the '
+                    'device to make loading faster and data fetching while offline possible. '
+                    'If you need to refresh the cache, clear the respective caches below. '
+                    'If data errors are encountered, try clearing the respective cache below.',
+                maxLines: 10,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+    items.add(
+      Padding(
+        padding: const EdgeInsets.only(
+          left: 25,
+          right: 25,
+          top: 10,
+        ),
+        child: Row(
+          children: [
+            OutlinedButton(
+              child: Text(
+                'Staff Cache',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
+              onPressed: () async {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('Clearing cache...'),
+                  ),
+                );
+                await ClassesCacheManager().emptyCache();
+                await StaffCacheManager().emptyCache();
+                await StaffImageCacheManager().emptyCache();
+                ScaffoldMessenger.of(context).clearSnackBars();
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: const Text('Cache Cleared'),
+                    duration: const Duration(
+                      seconds: 3,
+                    ),
+                    action: SnackBarAction(
+                      label: 'Ok',
+                      onPressed: () {},
+                    ),
+                  ),
+                );
+              },
+            )
+          ],
+        ),
+      ),
+    );
+    items.add(
+      Padding(
+        padding: const EdgeInsets.only(
+          left: 25,
+          right: 25,
+        ),
+        child: Row(
+          children: [
+            OutlinedButton(
+              child: Text(
+                'Events Cache',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
+              onPressed: () async {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('Clearing cache...'),
+                  ),
+                );
+                await EventsCacheManager().emptyCache();
+                ScaffoldMessenger.of(context).clearSnackBars();
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: const Text('Cache Cleared'),
+                    duration: const Duration(
+                      seconds: 3,
+                    ),
+                    action: SnackBarAction(
+                      label: 'Ok',
+                      onPressed: () {},
+                    ),
+                  ),
+                );
+              },
+            )
+          ],
+        ),
+      ),
+    );
+    items.add(
+      Padding(
+        padding: const EdgeInsets.only(
+          left: 25,
+          right: 25,
+        ),
+        child: Row(
+          children: [
+            OutlinedButton(
+              child: Text(
+                'Menu Cache',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
+              onPressed: () async {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('Clearing cache...'),
+                  ),
+                );
+                await MenuCacheManager().emptyCache();
+                await MenuImageCacheManager().emptyCache();
+                ScaffoldMessenger.of(context).clearSnackBars();
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: const Text('Cache Cleared'),
+                    duration: const Duration(
+                      seconds: 3,
+                    ),
+                    action: SnackBarAction(
+                      label: 'Ok',
+                      onPressed: () {},
+                    ),
+                  ),
+                );
+              },
+            )
+          ],
+        ),
+      ),
+    );
+    items.add(
+      const SizedBox(
+        height: 25,
       ),
     );
     return items;
